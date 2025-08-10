c830001017be14f5642968692308d47eaf01b6ed
Daria Rosen <daria.sk135@gmail.com>
2025-08-10 12:19:27 +0300

    add corsOptions

--- Express-server/server.js
+++ Express-server/server.js
@@ -2,9 +2,21 @@ import express from 'express'
 import { makeId } from './services/util.service.js'
 import { bugService } from './services/bug.service.js'
 import { loggerService } from './services/logger.service.js'
+import cors from 'cors'
 
 const app = express()
 app.use(express.static('public'))
+
+const corsOptions = {
+    origin: [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'http://localhost:5174',
+        'http://127.0.0.1:5174',
+    ],
+    credentials: true,
+}
+app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
     res.send('Hello, World!!!')

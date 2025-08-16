5e5195d4cafd247f18eb85b71a1e1ab7d9df1965
Daria Rosen <daria.sk135@gmail.com>
2025-08-16 16:11:47 +0300

    Add users and refactor some user backend

--- Express-server/api/user/user.routes.js
+++ Express-server/api/user/user.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { getUser, getUsers, deleteUser, updateUser } from './user.controller.js'
+import { addUser, getUser, removeUser, updateUser, getUsers} from './user.controller.js'
 
 const router = express.Router()
 
 router.get('/', getUsers)
 router.get('/:id', getUser)
+router.delete('/:id', removeUser)
+router.post('/', addUser)
 router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
 
 export const userRoutes = router

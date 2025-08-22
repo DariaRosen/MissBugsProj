5e316da1daadde496838229a0f599d14961e4629
Daria Rosen <daria.sk135@gmail.com>
2025-08-22 12:35:06 +0300

    bug fixing

--- Express-server/public/New folder/js/bugs.js
+++
@@ -1,9 +0,0 @@
-console.log('Hello, World!');
-
-async function onGetBugs() {
-    const elBugList = document.querySelector('pre')
-    const res = await fetch('/api/bug')
-    const bugs = await res.json()
-
-    elBugList.innerText = JSON.stringify(bugs, null, 4)
-}

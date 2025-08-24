b685a8e992b67c30d9a4ab5ba341a2413b30a923
Daria Rosen <daria.sk135@gmail.com>
2025-08-24 12:43:59 +0300

    Add prewiew service and refactor index.js support MongoDB

--- miss-bug-starter-react-vite/src/services/bug/index.js
+++ miss-bug-starter-react-vite/src/services/bug/index.js
@@ -1,6 +1,36 @@
+const { DEV, VITE_LOCAL } = import.meta.env
+import { getRandomIntInclusive, makeId } from "../service.utils"
+
 import { bugService as remoteService } from "./bug.service";
 import { bugService as localService } from "./bug.service.local";
 
-const isRemote = true
+function getEmptyBug() {
+  return {
+    id: makeId(),
+    title: '',
+    description: '',
+    severity: getRandomIntInclusive(1, 5),
+    labels: [],
+    creator: '',
+  };
+}
 
-export const bugService = isRemote ? remoteService : localService
+function getDefaultFilter() {
+  return {
+    severity: '',
+    title: '',
+    createdAt: '',
+  };
+}
+
+console.log('VITE_LOCAL:', VITE_LOCAL)
+
+
+const service = VITE_LOCAL === 'true' ? localService : remoteService
+export const bugService = { getEmptyBug, getDefaultFilter, ...service }
+
+
+
+// const isRemote = true
+
+// export const bugService = isRemote ? remoteService : localService

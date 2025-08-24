d37064cdd73eb46b3490a33eaca69500a3fd7cff
Daria Rosen <daria.sk135@gmail.com>
2025-08-24 12:43:59 +0300

    Add prewiew service and refactor index.js support MongoDB

--- miss-bug-starter-react-vite/src/services/user/index.js
+++ miss-bug-starter-react-vite/src/services/user/index.js
@@ -1,6 +1,30 @@
+const { DEV, VITE_LOCAL } = import.meta.env
+import { getRandomIntInclusive, makeId } from "../service.utils"
+
 import { userService as remoteService } from "./user.service";
 import { userService as localService } from "./user.service.local";
 
-const isRemote = true
+function getEmptyUser() {
+    return {
+        username: '',
+        password: '',
+        fullname: '',
+        isAdmin: false,
+        score: 100,
+    };
+}
 
-export const userService = isRemote ? remoteService : localService
+// MongoDB
+const service = VITE_LOCAL === 'true' ? localService : remoteService
+export const userService = { ...service, getEmptyUser }
+
+// Easy access to this service from the dev tools console
+// when using script - dev / dev:local
+
+if (DEV) window.userService = userService
+
+
+// render.com
+// const isRemote = true
+
+// export const userService = isRemote ? remoteService : localService

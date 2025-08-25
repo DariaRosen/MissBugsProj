9f50885db7b384aa10fe91c26239493f776c3ff4
Daria Rosen <daria.sk135@gmail.com>
2025-08-25 12:31:55 +0300

    build

--- miss-bug-starter-react-vite/src/services/bug/index.js
+++ miss-bug-starter-react-vite/src/services/bug/index.js
@@ -1,36 +1,36 @@
-const { DEV, VITE_LOCAL } = import.meta.env
-import { getRandomIntInclusive, makeId } from "../service.utils"
+// const { DEV, VITE_LOCAL } = import.meta.env
+// import { getRandomIntInclusive, makeId } from "../service.utils"
 
 import { bugService as remoteService } from "./bug.service";
 import { bugService as localService } from "./bug.service.local";
 
-function getEmptyBug() {
-  return {
-    id: makeId(),
-    title: '',
-    description: '',
-    severity: getRandomIntInclusive(1, 5),
-    labels: [],
-    creator: '',
-  };
-}
+// function getEmptyBug() {
+//   return {
+//     id: makeId(),
+//     title: '',
+//     description: '',
+//     severity: getRandomIntInclusive(1, 5),
+//     labels: [],
+//     creator: '',
+//   };
+// }
 
-function getDefaultFilter() {
-  return {
-    severity: '',
-    title: '',
-    createdAt: '',
-  };
-}
+// function getDefaultFilter() {
+//   return {
+//     severity: '',
+//     title: '',
+//     createdAt: '',
+//   };
+// }
 
-console.log('VITE_LOCAL:', VITE_LOCAL)
-
-
-const service = VITE_LOCAL === 'true' ? localService : remoteService
-export const bugService = { getEmptyBug, getDefaultFilter, ...service }
+// console.log('VITE_LOCAL:', VITE_LOCAL)
 
 
+// const service = VITE_LOCAL === 'true' ? localService : remoteService
+// export const bugService = { getEmptyBug, getDefaultFilter, ...service }
 
-// const isRemote = true
 
-// export const bugService = isRemote ? remoteService : localService
+
+const isRemote = true
+
+export const bugService = isRemote ? remoteService : localService
--- miss-bug-starter-react-vite/src/services/user/index.js
+++ miss-bug-starter-react-vite/src/services/user/index.js
@@ -1,30 +1,30 @@
-const { DEV, VITE_LOCAL } = import.meta.env
-import { getRandomIntInclusive, makeId } from "../service.utils"
+// const { DEV, VITE_LOCAL } = import.meta.env
+// import { getRandomIntInclusive, makeId } from "../service.utils"
 
 import { userService as remoteService } from "./user.service";
 import { userService as localService } from "./user.service.local";
 
-function getEmptyUser() {
-    return {
-        username: '',
-        password: '',
-        fullname: '',
-        isAdmin: false,
-        score: 100,
-    };
-}
+// function getEmptyUser() {
+//     return {
+//         username: '',
+//         password: '',
+//         fullname: '',
+//         isAdmin: false,
+//         score: 100,
+//     };
+// }
 
 // MongoDB
-const service = VITE_LOCAL === 'true' ? localService : remoteService
-export const userService = { ...service, getEmptyUser }
+// const service = VITE_LOCAL === 'true' ? localService : remoteService
+// export const userService = { ...service, getEmptyUser }
 
 // Easy access to this service from the dev tools console
 // when using script - dev / dev:local
 
-if (DEV) window.userService = userService
+// if (DEV) window.userService = userService
 
 
 // render.com
-// const isRemote = true
+const isRemote = true
 
-// export const userService = isRemote ? remoteService : localService
+export const userService = isRemote ? remoteService : localService
